94df90e6e3d66fdb134acef929e05aecd8af3b3f
Emranshaikk <imran22shaik2022@outlook.com>
2023-07-26 14:17:31 +0530

    Javascript hindi

---
+++ 01_variables.js
@@ -0,0 +1,38 @@
+const accountId = 435555
+let accountEmail = "imran22shaik@gmail.com"
+var accountPassword = "880107"   
+accountCity = "Jaipur"
+let accountState;
+
+// prefer not to use var//
+
+console.log(accountId);
+console.table([accountId, accountEmail, accountPassword, accountCity, accountState])
+
+const accountId2 = "456545"
+let accountEmail2 = "imran222shaik"
+let accountPassword2 = "123456"
+let accountCity2 = "Hyderabad"
+let accountState1
+
+console.table ([accountId2,accountEmail2,accountPassword2,accountCity2,accountState1])
+
+const accountId3 = "4563545"
+const accountEmail20 = "imran2222shaik"
+const accountPassword20 = "1234567"
+const accountCity20 = "Hyderabaad"
+
+
+console.table ([accountId3,accountEmail20,accountPassword20,accountCity20])
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 02_dataTypes.js
@@ -0,0 +1,22 @@
+"Use Strict"; // treat all the JS code as newer version
+// alert(3+3)// we are using nodejs, not browser
+// code readability should be high //
+
+let name = "Shaik"
+let age = "30"
+let city = "Hyderabad"
+let isLoggedIn = false
+let state;
+
+
+// number => 2 to power 53
+// bigint
+// string => ""
+// boolean => true/false
+// null => standalone value
+// undefined =>
+// symbol => unique
+
+// object
+
+console.log(typeof city);
