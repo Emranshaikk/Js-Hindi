a81bd5af99b13dc89f5a3d3e53c6c20aac75d5b9
Emranshaikk <imran22shaik2022@outlook.com>
2023-07-27 00:37:34 +0530

    v.1

--- 03_conversionOperation.js
+++ 03_conversionOperation.js
@@ -7,9 +7,6 @@ let valueInNumber = Number(score)
 //console.log(typeof valueInNumber);
 //console.log(valueInNumber);
 
-
-
-
 // "33" => 33
 // "33abc" => NaN
 // true => 1; false => 0
@@ -18,7 +15,7 @@ let valueInNumber = Number(score)
 let isLoggedIn = 1
 
 let booleanIsLoggedIn = Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn);
+//console.log(booleanIsLoggedIn);
 
 
 // 1 => true; 0 => false
@@ -27,5 +24,24 @@ console.log(booleanIsLoggedIn);
 
 let someNumber = 33
 let stringNumber = String(someNumber)
-console.log(stringNumber)
-console.log(typeof stringNumber)
+
+//console.log(stringNumber)
+//console.log(typeof stringNumber)
+
+
+// *** Operations ***//
+
+let value = 3
+let negValue = -value
+console.log(negValue);
+console.log(2**3);
+
+let str1 = 15
+let str2 = 35
+let str3 = str1+str2
+console.log(str3);
+
+console.log("1" + 2);
+console.log(1 + "2");
+console.log("1" + 2 + 2);
+console.log(1 + 2 + "2");
