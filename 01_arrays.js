c779dd44fb89a0bfa0ed8c4f6563ee829d9a3fd4
Emranshaikk <imran22shaik2022@outlook.com>
2023-07-29 11:48:03 +0530

    objects

---
+++ 01_arrays.js
@@ -0,0 +1,57 @@
+// arrays
+// {} Curly Braces
+// [] Brackets
+// () Parenthesis\ 
+// (Elements Inside the brackets)
+// Variables = Const myArray
+//Shallow Copies
+//Deep Copies
+
+const myArray = [1, 2, 3, 4, 5,]
+//console.log(myArray[1]);
+
+// Array Methods
+
+//myArray.push(11)
+//myArray.push(12) // will add numbers to array
+//myArray.pop() // will remove the last numb
+
+//myArray.unshift(9)
+//myArray.shift()
+
+//console.log(myArray.includes(9));
+//console.log(myArray.indexOf(19));
+//console.log(myArray);
+
+//const mynewArray = myArray.join()
+//console.log(myArray);    
+//console.log(typeof mynewArray);
+
+// ******* Slice and Splice ******* //
+
+console.log("A", myArray);
+
+const myn1 = myArray.slice(1,3)
+console.log(myn1);
+
+console.log("B", myArray);
+
+const myn2 = myArray.splice(1,3)
+console.log(myn2);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ 07_dates.js
@@ -0,0 +1,39 @@
+//Dates
+
+//let myDate = new Date()
+// console.log(myDate. toString());
+// console.log(myDate. toLocaleDateString());
+// console.log(typeof myDate);
+
+//let myCreateDate = new Date(2023, 0, 23)
+//let myCreateDate = new Date(2023, 0, 23, 5, 3)
+
+// let myCreatedDate = new Date("1-14-2024")
+// console.log(myCreatedDate. toLocaleString());
+
+
+//let myTimeStamp = Date.now()
+//console.log(myTimeStamp);
+
+//console.log(Math.floor(Date.now()/1000));
+
+let newDate = new Date()
+console.log(newDate);
+console.log(newDate.getMonth());
+
+newDate. toLocaleString ('default',{
+    weekday: "long",
+
+})
+
+console.log(newDate);
+
+
+
+
+
+
+
+
+
+
